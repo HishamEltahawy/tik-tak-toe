68d765caafb5e179494cc9f5175618c33826bfe9
hisham <hishameltahawy555@gmail.com>
2024-09-05 17:37:16 +0300

    finish class player

---
+++ new_oop2.py
@@ -0,0 +1,24 @@
+class Player():
+    def __init__(self, name, symbol) -> None:
+        self.name = name
+        self.symbol = symbol
+    def choose_name(self):
+        while True:
+            choice = input('Type Your Name:')
+            if choice.isalpha():
+                self.name = choice
+                break
+            else:
+                print("Invalid Entry Try Again")
+    def choose_symbol(self):
+        while True:
+            choice = input('Typer Your Symbol:').upper()
+            if len(choice) == 1 and choice.isdigit():
+                self.symbol = choice
+                break
+            else:
+                print("Invalid Choice Just Single Letter")
+        
+
+
+
