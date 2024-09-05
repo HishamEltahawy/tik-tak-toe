053a88a617034b8c28296fd6e8345aecdc884565
hisham <hishameltahawy555@gmail.com>
2024-09-05 18:23:13 +0300

    finish board class

--- new_oop2.py
+++ new_oop2.py
@@ -19,6 +19,30 @@ class Player():
             else:
                 print("Invalid Choice Just Single Letter")
         
+class Board():  
+    def __init__(self, board) -> None:
+        self.board = [str(i) for i in range (1, 10)]
 
+    def show_board(self):
+        for i in range(0, 9, 3):
+            print("|".join(self.board[i:i+3]))
+
+    def update_board(self, symbol, choice):
+        if self.is_valid_choice(choice):
+            self.board[choice-1] = symbol
+            return True
+        else:
+            print("<< You can't play here >>")
+            return False        
+
+    def is_valid_choice(self, choice):
+        return self.board[choice-1].isdigit()
+    
+    def reset_game(self):
+        self.board = [str(i) for i in range (1, 10)]
+
+
+
+    
 
 
